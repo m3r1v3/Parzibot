00dc14cfbc84a5c1a91dbb2c6ae8b225c09047d9
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-05-21 12:18:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import os
 
 from discord.ext import commands
+from cogs.language import get_lang
 
 client = commands.Bot(command_prefix='!')
 
@@ -20,6 +21,16 @@ async def unload(extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        if get_lang() == "RU":
+            await ctx.send('Команда не распознана.')
+        else:
+            await ctx.send('Command not recognized.')
+
 
 token = os.environ.get('BOT_TOKEN')
 client.run(str(token))
