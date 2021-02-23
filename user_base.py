95bae01b4e55a8a861c06b6af0f7ee25911d902d
Max Zaytsev <maxim.zaytsev.1305@gmail.com>
2021-02-23 14:34:55 +0300

    Update few files

--- user_base.py
+++ user_base.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 engine = create_engine(os.environ.get('DATABASE_URL'), echo=True)
 Base = declarative_base()
