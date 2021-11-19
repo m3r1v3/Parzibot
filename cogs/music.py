ab48220786cf0c0250a8996538bcf9fc19728ce9
merive <maxim.zaytsev.1305@gmail.com>
2021-11-19 17:22:13 +0300

    Was refactored Bot Classes
    * Moved some methods in alphabet order
    * Rewrote some methods

--- cogs/music.py
+++ cogs/music.py
@@ -1,10 +1,8 @@
 import asyncio
+import random
 
 import discord
 import youtube_dl
-
-import random
-
 from discord.ext import commands
 from discord_slash import cog_ext
 from discord_slash.utils.manage_commands import create_option
@@ -28,7 +26,7 @@ class Music(commands.Cog):
             await channel.connect()
             await ctx.send("**Parzibot** connected to **Voice Channel**")
         else:
-            await ctx.send("**Parzibot** already connected to  **Voice Channel**")            
+            await ctx.send("**Parzibot** already connected to  **Voice Channel**")
 
     @cog_ext.cog_slash(name="leave", description="Leave from Voice Channel")
     async def leave(self, ctx):
@@ -38,23 +36,24 @@ class Music(commands.Cog):
                 ctx.author.voice.channel == ctx.voice_client.channel or voice.is_connected() is None):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
         else:
-            self.queue = []
-            self.current = ""
+            self.queue, self.current = [], ""
             await voice.disconnect()
             await ctx.send("**Parzibot** left **Voice Channel**")
 
     @cog_ext.cog_slash(name="musicclear", description="Clear music queue")
     async def musicclear(self, ctx):
         """Clear music queue"""
-        if ctx.author.voice.channel is None or not ctx.author.voice.channel == ctx.voice_client.channel:
+        if (
+                ctx.author.voice.channel is None
+                or ctx.author.voice.channel != ctx.voice_client.channel
+        ):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
+        elif not self.queue:
+            self.queue = []
+            discord.utils.get(self.client.voice_clients, guild=ctx.guild).stop()
+            await ctx.send("**The Queue** has been cleared")
         else:
-            if not self.queue != []: 
-                self.queue = []
-                discord.utils.get(self.client.voice_clients, guild=ctx.guild).stop()
-                await ctx.send("**The Queue** has been cleared")  
-            else:
-                await ctx.send("**The Queue** has already been cleared")
+            await ctx.send("**The Queue** has already been cleared")
 
     @cog_ext.cog_slash(name="musichelp", description="List of Parzibot Music Commands")
     async def musichelp(self, ctx):
@@ -80,13 +79,16 @@ class Music(commands.Cog):
     @cog_ext.cog_slash(name="next", description="Play next song in queue")
     async def next(self, ctx):
         """Play next song in queue"""
-        if ctx.author.voice.channel is None or not ctx.author.voice.channel == ctx.voice_client.channel:
+        if (
+                ctx.author.voice.channel is None
+                or ctx.author.voice.channel != ctx.voice_client.channel
+        ):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
             return
         voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
         voice.stop()
 
-        await self.play_song(ctx)  
+        await self.play_song(ctx)
 
     @cog_ext.cog_slash(name="pause", description="Set Song on Pause")
     async def pause(self, ctx):
@@ -107,14 +109,17 @@ class Music(commands.Cog):
         """Play Song in Voice Channel"""
         voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
         channel = ctx.author.voice.channel
-        if not voice is None and not ctx.author.voice.channel == ctx.voice_client.channel:
+        if (
+                voice is not None
+                and ctx.author.voice.channel != ctx.voice_client.channel
+        ):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
             return
 
         self.queue.append(str(url))
 
         if channel:
-            if not voice is None and not voice.is_connected() is None:
+            if voice is not None and voice.is_connected() is not None:
                 await voice.move_to(channel)
             else:
                 voice = await channel.connect()
@@ -134,10 +139,11 @@ class Music(commands.Cog):
 
         voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
 
-        if self.queue != []:
+        if self.queue:
             self.current = self.queue.pop(0)
             data = search(self.current)
-            voice.play(discord.FFmpegPCMAudio(data['source'], **self.FFMPEG_OPTIONS), after=lambda e: asyncio.run_coroutine_threadsafe(self.play_song(ctx), self.client.loop))
+            voice.play(discord.FFmpegPCMAudio(data['source'], **self.FFMPEG_OPTIONS),
+                       after=lambda e: asyncio.run_coroutine_threadsafe(self.play_song(ctx), self.client.loop))
             voice.is_playing()
             await ctx.send(f"**{data['title']}** is playing")
         else:
@@ -146,7 +152,10 @@ class Music(commands.Cog):
     @cog_ext.cog_slash(name="replay", description="Replay current Song")
     async def replay(self, ctx):
         """Replay last sound"""
-        if ctx.author.voice.channel is None or not ctx.author.voice.channel == ctx.voice_client.channel:
+        if (
+                ctx.author.voice.channel is None
+                or ctx.author.voice.channel != ctx.voice_client.channel
+        ):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
             return
         voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
@@ -161,9 +170,10 @@ class Music(commands.Cog):
             return {'source': info['formats'][0]['url'], 'title': info['title']}
 
         voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-            
+
         data = search(self.current)
-        voice.play(discord.FFmpegPCMAudio(data['source'], **self.FFMPEG_OPTIONS), after=lambda e: asyncio.run_coroutine_threadsafe(self.play_song(ctx), self.client.loop))
+        voice.play(discord.FFmpegPCMAudio(data['source'], **self.FFMPEG_OPTIONS),
+                   after=lambda e: asyncio.run_coroutine_threadsafe(self.play_song(ctx), self.client.loop))
         voice.is_playing()
         await ctx.send(f"**{data['title']}** is playing")
 
@@ -183,14 +193,16 @@ class Music(commands.Cog):
     @cog_ext.cog_slash(name="shuffle", description="Shuffle list of songs")
     async def shuffle(self, ctx):
         """Shuffle list of songs"""
-        if ctx.author.voice.channel is None or not ctx.author.voice.channel == ctx.voice_client.channel:
+        if (
+                ctx.author.voice.channel is None
+                or ctx.author.voice.channel != ctx.voice_client.channel
+        ):
             await ctx.send("**Parzibot** isn't connected to your **Voice Channel**")
+        elif self.queue:
+            random.shuffle(self.queue)
+            await ctx.send("**The Queue** has been shuffled")
         else:
-            if self.queue != []:
-                random.shuffle(self.queue)
-                await ctx.send("**The Queue** has been shuffled")
-            else:
-                await ctx.send("**The Queue** is empty")       
+            await ctx.send("**The Queue** is empty")
 
     @cog_ext.cog_slash(name="stop", description="Stop current Song")
     async def stop(self, ctx):
