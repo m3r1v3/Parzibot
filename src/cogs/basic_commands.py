d5b57e8b991996c1ab5d778b4fe3ad3da476331b
Max Meriberg <merive.official@gmail.com>
2022-07-23 14:41:41 +0300

    style: update descriptions in basic_commands.py

--- src/cogs/basic_commands.py
+++ src/cogs/basic_commands.py
@@ -14,52 +14,49 @@ class BasicCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @cog_ext.cog_slash(name="about", description="Information About Parzibot")
+    @cog_ext.cog_slash(name="about", description="Information about Parzibot")
     async def about(self, ctx):
-        """Information About Parzibot"""
+        """Information about Parzibot"""
         await Message.msg(ctx, "About Parzibot", (
             f"**Parzibot** is free open-source project, created by **merive_**\n"
             f"You can find more information on [Parzibot Website](https://merive.herokuapp.com/Parzibot)\n"
-            f"**Parzibot**, {datetime.datetime.now().year}"
-            ))
+            f"**Parzibot**, {datetime.datetime.now().year}"))
 
     @cog_ext.cog_slash(
         name="clear",
-        description="Clear Messages in Current Text Channel",
+        description="Clear messages in current Text Channel",
         options=[
             create_option(
                 name="number",
-                description="Number of Messages for Clear",
+                description="Number of messages to be cleaned (Optional / Default 5)",
                 option_type=4,
                 required=False)
             ])
     async def clear(self, ctx, number=5):
-        """Clear Messages in Current Text Channel"""
+        """Clear messages in current Text Channel"""
         if number > 0:
             await ctx.channel.purge(limit=number)
             await Message.msg(ctx, "Messages has been cleared", f"Cleared **{number}** messages")
         else: await Message.error(ctx, "Clear error", f"Cannot clear **{number}** messages")
 
-
-    @cog_ext.cog_slash(name="help", description="The List of Parzibot Commands")
+    @cog_ext.cog_slash(name="help", description="The list of Parzibot basic commands")
     async def help(self, ctx):
-        """The List of Parzibot Commands"""
-        await Message.msg(ctx, "Bot Commands", (
-            " • **/about** - Information About Parzibot\n"
-            " • **/clear** `number` - Clear Messages in Current Text Channel\n"
-            " • **/help** - The list of Parzibot commands\n"
+        """The list of Parzibot basic commands"""
+        await Message.msg(ctx, "Parzibot // Basic Commands", (
+            " • **/about** - Information about Parzibot\n"
+            " • **/clear** `number` - Clear messages in current Text Channel\n"
+            " • **/help** - The list of Parzibot basic commands\n"
             " • **/ping** - Parzibot ping\n"
-            " • **/users** - List of Text Channel members\n"))
+            " • **/users** - The list of current Text Channel members\n"))
         
-
-    @cog_ext.cog_slash(name="ping", description="Parzibot's Ping")
+    @cog_ext.cog_slash(name="ping", description="Parzibot ping")
     async def ping(self, ctx):
-        """Parzibot"s Ping"""
+        """Parzibot ping"""
         await Message.msg(ctx, "Parzibot Ping", f"**Parzibot** Ping: `{round(self.client.latency * 1000)}ms`")
 
-    @cog_ext.cog_slash(name="users", description="List of Text Channel members")
+    @cog_ext.cog_slash(name="users", description="The list of current Text Channel members")
     async def users(self, ctx):
-        """The List of Text Channel Members"""
+        """The list of current Text Channel members"""
         await Message.msg(ctx, "Channel Members", "".join(f"\t**{member}**\n" for member in ctx.channel.members))
 
 
