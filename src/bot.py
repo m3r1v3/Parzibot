327143dd474540167b833481d5ef474de128a0e2
Max Meriberg <merive.official@gmail.com>
2022-09-22 23:18:30 +0300

    fix: update bot.py

--- src/bot.py
+++ src/bot.py
@@ -9,9 +9,13 @@ from discord.ext import commands
 class Bot(commands.Bot):
 
     def __init__(self):
+        intents = discord.Intents.all()
+        intents.message_content = True
+        intents.voice_states = True
+
         super().__init__(
             command_prefix='$',
-            intents = discord.Intents.all()
+            intents = intents            
             )
 
         self.cogs_extensions = [f'cogs.{filename[:-3]}' for filename in os.listdir('src/cogs') if filename.endswith('.py')]
