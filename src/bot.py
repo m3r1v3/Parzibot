5c0f74a71dea53fb1a4a1924d01932709b6131e2
Max Meriberg <merive.official@gmail.com>
2022-09-24 13:36:51 +0300

    build: update requirements.txt

--- src/bot.py
+++ src/bot.py
@@ -9,13 +9,9 @@ from discord.ext import commands
 class Bot(commands.Bot):
 
     def __init__(self):
-        intents = discord.Intents.all()
-        intents.message_content = True
-        intents.voice_states = True
-
         super().__init__(
             command_prefix='$',
-            intents = intents            
+            intents = discord.Intents.all()           
             )
 
         self.cogs_extensions = [f'cogs.{filename[:-3]}' for filename in os.listdir('src/cogs') if filename.endswith('.py')]
