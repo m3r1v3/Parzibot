e83cf03b38920489118fd3ac0655dbca56aa6659
Max Meriberg <merive.official@gmail.com>
2022-09-27 20:01:13 +0300

    feat: add commands disabling

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+
 class Bot(commands.Bot):
 
     def __init__(self):
@@ -17,5 +18,22 @@ class Bot(commands.Bot):
             await self.load_extension(ext)
         await bot.tree.sync()
 
+    def music_disable(bot: commands.Bot):
+        if int(os.environ.get('MUSIC_COMMANDS')):
+            bot.remove_command(name="musichelp")
+            bot.remove_command(name="connect")
+            bot.remove_command(name="disconnect")
+            bot.remove_command(name="play")
+            bot.remove_command(name="replay")
+            bot.remove_command(name="pause")
+            bot.remove_command(name="resume")
+            bot.remove_command(name="repeat")
+            bot.remove_command(name="next")
+            bot.remove_command(name="playlist")
+            bot.remove_command(name="playlistadd")
+            bot.remove_command(name="playlistclear")
+            bot.remove_command(name="playlistshuffle")
+
 bot = Bot()
 bot.run(str(os.environ.get('BOT_TOKEN')))
+bot.music_disable() # Delete if doesn't need disable music commands
