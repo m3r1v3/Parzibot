4e42e778b2fedefee59ec527d6d410dd10dacd68
Max Meriberg <merive.official@gmail.com>
2022-09-27 20:06:39 +0300

    fix: remove music_disable method

--- src/bot.py
+++ src/bot.py
@@ -18,22 +18,5 @@ class Bot(commands.Bot):
             await self.load_extension(ext)
         await bot.tree.sync()
 
-    def music_disable(bot: commands.Bot):
-        if int(os.environ.get('MUSIC_COMMANDS')):
-            bot.remove_command(name="musichelp")
-            bot.remove_command(name="connect")
-            bot.remove_command(name="disconnect")
-            bot.remove_command(name="play")
-            bot.remove_command(name="replay")
-            bot.remove_command(name="pause")
-            bot.remove_command(name="resume")
-            bot.remove_command(name="repeat")
-            bot.remove_command(name="next")
-            bot.remove_command(name="playlist")
-            bot.remove_command(name="playlistadd")
-            bot.remove_command(name="playlistclear")
-            bot.remove_command(name="playlistshuffle")
-
 bot = Bot()
 bot.run(str(os.environ.get('BOT_TOKEN')))
-bot.music_disable() # Delete if doesn't need disable music commands
