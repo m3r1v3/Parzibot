5dd9db2640b429bb566bfe9a529dbd39f30d78c3
Max Meriberg <merive.official@gmail.com>
2022-10-16 18:37:00 +0300

    refactor: event_handler.py

--- src/cogs/event_handler.py
+++ src/cogs/event_handler.py
@@ -1,8 +1,8 @@
 import discord
-from discord import app_commands, Embed, Colour
 from discord.ext import commands
 
 from message import Message
+
 
 class EventHandler(commands.Cog):
 
@@ -15,17 +15,20 @@ class EventHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        await Message.basic_msg(member, f"Parzibot // Hey **{member}**!", "White **/help** to find out basic command, **/gamehelp** to find out game commands or **/musichelp** to find out music command")
+        await Message.basic_msg(member, f"Parzibot // Hey **{member}**!",
+                                "White **/help** to find out basic command, **/gamehelp** to find out game commands or **/musichelp** to find out music command")
         await member.add_roles(discord.utils.get(member.guild.roles, id=int(Role().get_role(member.guild.id))))
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         if len(before.roles) < len(after.roles):
-            for i in after.roles: 
-                if i not in before.roles: await Message.basic_msg(before, "Parzibot // Received role", f"You has been received **{i} Role**")
+            for i in after.roles:
+                if i not in before.roles: await Message.basic_msg(before, "Parzibot // Received role",
+                                                                  f"You has been received **{i} Role**")
         elif len(before.roles) > len(after.roles):
             for i in before.roles:
-                if i not in after.roles: await Message.basic_msg(before, "Parzibot // Deprived role", f"You has been deprived **{i} Role**")
+                if i not in after.roles: await Message.basic_msg(before, "Parzibot // Deprived role",
+                                                                 f"You has been deprived **{i} Role**")
 
 
 async def setup(bot: commands.Bot) -> None:
