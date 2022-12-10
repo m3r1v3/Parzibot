da661dfc47d29f6a4ccddd3cabe91754af9fb5a4
Max Meriberg <merive.official@gmail.com>
2022-12-10 16:01:53 +0300

    Connect basic commands to message.json
    :sparkles: feat: Connect basic commands to message.json
    :sparkles: feat: Update message.json
    :sparkles: feat: Add get_basic_msg() and get_error_msg() methods in Message class

--- src/message.py
+++ src/message.py
@@ -1,3 +1,5 @@
+import json
+
 import discord
 
 from discord import Embed, Colour
@@ -6,52 +8,55 @@ from discord.ext import commands
 
 class Message:
 
-    @staticmethod
     def get_embed(title: str, description: str):
         return Embed(title=title, description=description, color=Colour(0x68FFD9)).set_thumbnail(
             url="attachment://Parzibot.png")
 
-    @staticmethod
     def get_games_embed(title: str, description: str):
         return Embed(title=title, description=description, color=Colour(0x8168ff)).set_thumbnail(
             url="attachment://ParzibotGames.png")
 
-    @staticmethod
     def get_music_embed(title: str, description: str):
         return Embed(title=title, description=description, color=Colour(0x68ff72)).set_thumbnail(
             url="attachment://ParzibotMusic.png")
 
-    @staticmethod
     def get_admin_embed(title: str, description: str):
         return Embed(title=title, description=description, color=Colour(0xffff68)).set_thumbnail(
             url="attachment://ParzibotAdmin.png")
 
-    @staticmethod
     def get_error_embed(title: str, description: str):
         return Embed(title=title, description=description, color=Colour(0xff6868)).set_thumbnail(
             url="attachment://ParzibotError.png")
 
     @staticmethod
+    def get_basic_msg(section, key):
+        return json.loads(open("message.json"))["basic"][section][key]
+
+    @staticmethod
+    def get_error_msg(section, key):
+        return json.loads(open("message.json"))["error"][section][key]
+
+    @staticmethod
     async def basic_msg(ctx, title: str, description: str):
         await ctx.send(file=discord.File("src/images/Parzibot.png", filename="Parzibot.png"),
-                       embed=Message.get_embed(title, description))
+                       embed=Message().get_embed(title, description))
 
     @staticmethod
     async def games_msg(ctx, title: str, description: str):
         await ctx.send(file=discord.File("src/images/ParzibotGames.png", filename="ParzibotGames.png"),
-                       embed=Message.get_games_embed(title, description))
+                       embed=Message().get_games_embed(title, description))
 
     @staticmethod
     async def music_msg(ctx, title: str, description: str):
         await ctx.send(file=discord.File("src/images/ParzibotMusic.png", filename="ParzibotMusic.png"),
-                       embed=Message.get_music_embed(title, description))
+                       embed=Message().get_music_embed(title, description))
 
     @staticmethod
     async def admin_msg(ctx, title: str, description: str):
         await ctx.send(file=discord.File("src/images/ParzibotAdmin.png", filename="ParzibotAdmin.png"),
-                       embed=Message.get_admin_embed(title, description))
+                       embed=Message().get_admin_embed(title, description))
 
     @staticmethod
     async def error_msg(ctx, title: str, message: str):
         await ctx.send(file=discord.File("src/images/ParzibotError.png", filename="ParzibotError.png"),
-                       embed=Message.get_error_embed(title, message))
+                       embed=Message().get_error_embed(title, message))
