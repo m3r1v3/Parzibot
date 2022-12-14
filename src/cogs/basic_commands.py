10380ef4f565b3fc4ae3a46d2877724af2e9ff83
Max Meriberg <merive.official@gmail.com>
2022-12-14 21:31:49 +0300

    Fix BasicCommands cog
    :bug: fix: Fix BasicCommands cog

--- src/cogs/basic_commands.py
+++ src/cogs/basic_commands.py
@@ -26,12 +26,14 @@ class BasicCommands(discord.ext.commands.Cog):
 
     @app_commands.command(name="about", description=Message.get_basic_msg("descriptions", "about"))
     async def about(self, ctx: discord.ext.commands.Context, interaction: discord.Interaction):
+        ctx: commands.Context = await self.bot.get_context(interaction)
         await Message.basic_msg(ctx, Message.get_basic_msg("titles", "about"),
             Message.get_basic_msg("messages", "about").format(year=BasicCommands.get_year(), changelog=BasicCommands.get_changelog(), version=BasicCommands.get_version()))
 
     @app_commands.command(name="clear", description=Message.get_basic_msg("descriptions", "clear"))
     @app_commands.describe(number=Message.get_basic_msg("descriptions", "clear-number"))
     async def clear(self, ctx: discord.ext.commands.Context, interaction: discord.Interaction, number: int = 5):
+        ctx: commands.Context = await self.bot.get_context(interaction)
         if number:
             await interaction.channel.purge(limit=number)
             await Message.basic_msg(ctx, Message.get_basic_msg("titles", "clear"), Message.get_basic_msg("messages", "clear").format(number=number))
@@ -39,14 +41,17 @@ class BasicCommands(discord.ext.commands.Cog):
 
     @app_commands.command(name="help", description=Message.get_basic_msg("descriptions", "help"))
     async def help(self, ctx: discord.ext.commands.Context, interaction: discord.Interaction):
+        ctx: commands.Context = await self.bot.get_context(interaction)
         await Message.basic_msg(ctx, Message.get_basic_msg("titles", "help"), Message.get_basic_msg("messages", "help"))
 
     @app_commands.command(name="ping", description=Message.get_basic_msg("descriptions", "ping"))
     async def ping(self, ctx: discord.ext.commands.Context, interaction: discord.Interaction):
+        ctx: commands.Context = await self.bot.get_context(interaction)
         await Message.basic_msg(ctx, Message.get_basic_msg("titles", "ping"), Message.get_basic_msg("messages", "ping").format(latency=round(self.bot.latency * 1000)))
 
     @app_commands.command(name="members", description=Message.get_basic_msg("descriptions", "members"))
     async def members(self, ctx: discord.ext.commands.Context, interaction: discord.Interaction):
+        ctx: commands.Context = await self.bot.get_context(interaction)
         await Message.basic_msg(ctx, Message.get_basic_msg("titles", "members"), "".join(f"\u2022 **{member}** ({member.status})\n" for member in interaction.channel.members))
 
 
